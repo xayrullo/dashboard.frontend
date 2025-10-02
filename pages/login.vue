7591aacebf7ed4c6d555e95c22a7518c0cd2486e
Xayrullo Jaloldinov <xayrullo.jaloldinov@gmail.com>
2025-10-03 01:29:39 +0500

    Added ui to the login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,34 @@
 <template>
-  <v-container class="h-full"> Login Page </v-container>
+  <v-container class="h-full">
+    <div
+      class="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24"
+    >
+      <div class="mx-auto w-full max-w-sm lg:w-96">
+        <div>
+          <h2 class="mt-8 text-2xl/9 font-bold tracking-tight text-center">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div class="mt-10">
+          <div>
+            <v-form class="space-y-3">
+              <v-text-field
+                label="Username"
+                placeholder="johndoe@gmail.com"
+                type="email"
+              />
+
+              <v-text-field label="Password" type="password" />
+              <v-btn class="w-full" size="large" color="success">
+                Sign in
+              </v-btn>
+            </v-form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </v-container>
 </template>
 
 <script setup lang="ts">
